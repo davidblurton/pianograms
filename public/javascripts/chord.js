5c7439200921ec4751342c0e07ee0c60e8a6db67
David Blurton <david.blurton@gmail.com>
2013-12-05 22:39:09 +0000

    Add function to set name and event to listen for changes

--- public/javascripts/chord.js
+++ public/javascripts/chord.js
@@ -1,11 +1,19 @@
 var Chord = Backbone.Model.extend({
   initialize: function(){
-    console.log('initialize model')
-  }
+    console.log('initialize model');
+
+    this.on('change:name', function(){
+        console.log('name changed.');
+    });
+  },
 
   defaults: function(){
     name: 'C7',
     notes: [0,1,2]
+  },
+
+  setName: function(newName){
+    this.set({ name: newName });
   }
 });
 
