8ff62f025f95b7af654eabc9b0873da9f47fde95
David Blurton <david.blurton@gmail.com>
2013-12-06 18:03:17 +0000

    Extract method

--- public/js/collections/keyboard.js
+++ public/js/collections/keyboard.js
@@ -5,8 +5,19 @@ var Keyboard = Backbone.Collection.extend({
   model: app.Key,
 
   initialize: function() {
+    this.createWhiteKeys();
+    this.createBlackKeys();
+  },
+
+  createWhiteKeys: function(){
     for(var index = 0; index < 10; index++){
       this.addKey(index * 23);
+    }
+  },
+
+  createBlackKeys: function(){
+    for(var index = 0; index < 10; index++){
+      this.addBlackKey((index * 23) + 23 / 2);
     }
   },
 
@@ -15,6 +26,14 @@ var Keyboard = Backbone.Collection.extend({
     this.add(model);
 
     var view = new app.WhiteKeyView({model: model}, x);
+    $('#keyboard').append( view.render().el );
+  },
+
+  addBlackKey: function(x){
+    var model = new app.Key();  
+    this.add(model);
+
+    var view = new app.BlackKeyView({model: model}, x);
     $('#keyboard').append( view.render().el );
   },
 });
