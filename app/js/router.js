773543a7b94c147a6cd4eca48f93e612b1f411eb
David Blurton <david.blurton@gmail.com>
2013-12-12 21:08:59 +0000

    parse note letters in to numbers

--- app/js/router.js
+++ app/js/router.js
@@ -13,14 +13,53 @@ define(['jquery',
       'chord/*query': 'setNotes'
     },
 
-    initialize: function(options){
+    initialize: function (options) {
       this.model = options.model;
     },
 
     setNotes: function (query) {
-      if(query){
+      if (query) {
         var notes = query.split(',');
-        this.model.set('notes', notes);
+
+        if (_.all(notes, function (note) {
+          return _.isNumber(note);
+        })) {
+          this.model.set('notes', notes);
+        } else {
+          var noteNames = {
+            'C': 0,
+            'C#': 1,
+            'Db': 1,
+            'D': 2,
+            'D#': 3,
+            'Eb': 3,
+            'E': 4,
+            'F': 5,
+            'F#': 6,
+            'Gb': 6,
+            'G': 7,
+            'G#': 8,
+            'Ab': 8,
+            'A': 9,
+            'A#': 10,
+            'Bb': 11,
+            'B': 11
+          };
+
+          var parsedNotes = notes.map(function (note) {
+            return noteNames[note];
+          });
+
+          // if next note less than previous add 12
+          for (var i = 1; i < parsedNotes.length; i++) {
+            if (parsedNotes[i] <= parsedNotes[i - 1]) {
+              parsedNotes[i] = parsedNotes[i] + 12; // shift the note up an octave
+            }
+          }
+
+          this.model.set('notes', parsedNotes);
+          // parse note names into numbers
+        }
       }
     }
   });
@@ -50,7 +89,7 @@ define(['jquery',
     });
 
     Backbone.history.start({
-      pushState: false
+      pushState: true
     });
   };
 
