8f00434a596a5313234826d11652c11878fb193d
David Blurton <david.blurton@gmail.com>
2013-12-12 23:35:41 +0000

    Extract methods in note converter

--- app/js/modules/NoteConverter.js
+++ app/js/modules/NoteConverter.js
@@ -1,3 +1,7 @@
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+
 define(function () {
   return {
     parseNotes: function (noteString) {
@@ -9,38 +13,59 @@ define(function () {
       if (numbersRegExp.test(noteString)) {
         return notes;
       } else if (lettersRegExp.test(noteString)) {
-        var noteNames = {
-          'C': 0,
-          'C#': 1,
-          'Db': 1,
-          'D': 2,
-          'D#': 3,
-          'Eb': 3,
-          'E': 4,
-          'F': 5,
-          'F#': 6,
-          'Gb': 6,
-          'G': 7,
-          'G#': 8,
-          'Ab': 8,
-          'A': 9,
-          'A#': 10,
-          'Bb': 11,
-          'B': 11
-        };
+        return this.parseLettersToNotes(notes);
+      } else {
+        return [];
+      }
+    },
 
-        var parsedNotes = notes.map(function (note) {
-          return noteNames[note];
-        });
+    parseLettersToNotes: function (letters) {
+      var noteNames = {
+        'C': 0,
+        'C#': 1,
+        'Db': 1,
+        'D': 2,
+        'D#': 3,
+        'Eb': 3,
+        'E': 4,
+        'F': 5,
+        'F#': 6,
+        'Gb': 6,
+        'G': 7,
+        'G#': 8,
+        'Ab': 8,
+        'A': 9,
+        'A#': 10,
+        'Bb': 10,
+        'B': 11
+      };
 
-        // if next note less than previous add 12
-        for (var i = 1; i < parsedNotes.length; i++) {
-          if (parsedNotes[i] <= parsedNotes[i - 1]) {
-            parsedNotes[i] = parsedNotes[i] + 12; // shift the note up an octave
-          }
+      var parsedNotes = letters.map(function (note) {
+        return noteNames[note];
+      });
+
+      while (!this.notesAreInOrder(parsedNotes)) {
+        this.orderNotes(parsedNotes);
+      }
+
+      return parsedNotes;
+    },
+
+    notesAreInOrder: function (notes) {
+      for (var i = 1; i < notes.length; i++) {
+        if (notes[i] <= notes[i - 1]) {
+          return false;
         }
+      }
 
-        return parsedNotes;
+      return true;
+    },
+
+    orderNotes: function (notes) {
+      for (var i = 1; i < notes.length; i++) {
+        if (notes[i] <= notes[i - 1]) {
+          notes[i] = notes[i] + 12; // shift the note up an octave
+        }
       }
     }
   };
