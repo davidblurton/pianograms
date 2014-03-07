faab3155f38d0dfe703e0157e6123371ed729b34
David Blurton <david.blurton@gmail.com>
2014-03-07 20:36:51 +0000

    Not using http module any more

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ require('newrelic');
 var express = require('express');
 var routes = require('./routes');
 var diagram = require('./routes/diagram');
-var http = require('http');
 var path = require('path');
 
 var app = express();
